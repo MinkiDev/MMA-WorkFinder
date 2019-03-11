361d22f8735a487b83f8fb4412ed6151320dc6e1
MinkiDev <minkidev@gmail.com>
2019-03-11 15:24:17 +0900

    Make resources embed to executable

---
+++ App.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace MMA_WorkFinder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(ResolveAssembly);
+        }
+
+        private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+            string name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+            if (resources.Any())
+            {
+                var resourceName = resources.First();
+                using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream == null)
+                        return null;
+                    var block = new byte[stream.Length];
+                    stream.Read(block, 0, block.Length);
+                    return Assembly.Load(block);
+                }
+            }
+            return null;
+        }
+    }
+}
